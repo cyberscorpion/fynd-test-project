47e3aec101ea253145521f59a48839df2640ca39
Rajat Jain <mznrajatjain@gmail.com>
2024-11-16 20:53:11 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from models import Base
 
 DATABASE_URL = "sqlite:///database.db"
 
-engine = create_engine('mysql+pymysql://pirayanshjain:fyndtest123@pirayanshjain.mysql.pythonanywhere-services.com:3306/fynd')
+engine = create_engine('mysql+pymysql://pirayanshjain:fyndtest123@pirayanshjain.mysql.pythonanywhere-services.com/pirayanshjain$fynd')
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind = engine)
